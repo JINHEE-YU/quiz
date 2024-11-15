51b7f1815004965f40bc55dbc98a92bc03283d06
JINHEE-YU <yujh2292@gmail.com>
2024-11-16 01:39:56 +0900

    💩 예외케이스 발견 - 로직 수정 필요

--- src/main/java/com/coding/quiz/solution/q_340213/Q340213.java
+++ src/main/java/com/coding/quiz/solution/q_340213/Q340213.java
@@ -9,11 +9,15 @@ public class Q340213 {
 
   public void execute() {
 
-    solution("34:33", "13:00", "00:55", "02:55", new String[] { "next", "prev" });
+    solution("30:00", "15:00", "15:10", "15:30", new String[] { "next", "prev" }); // "15:40"
 
-    solution("10:55", "00:05", "00:15", "06:55", new String[] { "prev", "next", "next" });
+    // solution("34:33", "13:00", "00:55", "02:55", new String[] { "next", "prev"
+    // });
 
-    solution("07:22", "04:05", "00:15", "04:07", new String[] { "next" });
+    // solution("10:55", "00:05", "00:15", "06:55", new String[] { "prev", "next",
+    // "next" });
+
+    // solution("07:22", "04:05", "00:15", "04:07", new String[] { "next" });
 
   }
 

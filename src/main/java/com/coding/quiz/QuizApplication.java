3e347a71662bb9f0b5930eb66cabc4f334d66dce
JINHEE-YU <yujh2292@gmail.com>
2024-11-16 00:57:15 +0900

    add "Q340212" 퍼즐 게임 챌린지 - 이진탐색

--- src/main/java/com/coding/quiz/QuizApplication.java
+++ src/main/java/com/coding/quiz/QuizApplication.java
@@ -3,11 +3,14 @@ package com.coding.quiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.coding.quiz.solution.q_340212.Q340212;
+
 @SpringBootApplication
 public class QuizApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuizApplication.class, args);
+		// SpringApplication.run(QuizApplication.class, args);
+		new Q340212().execute();
 	}
 
 }

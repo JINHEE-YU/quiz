600eeb39b195520db95132ab7c2f749cf48b7373
JINHEE-YU <yujh2292@gmail.com>
2024-11-19 19:14:17 +0900

    ✅ Q340213 테스트 코드 추가

--- src/main/java/com/coding/quiz/solution/q_340213/Q340213.java
+++ src/main/java/com/coding/quiz/solution/q_340213/Q340213.java
@@ -26,17 +26,3 @@ public class Q340213 implements QBase {
   }
 
 }
-
-@Slf4j
-class Solution {
-  String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-    String answer = "";
-
-    MovieTime mt = new MovieTime(video_len, pos, op_start, op_end);
-    mt.move(commands);
-
-    answer = mt.getStringPosT();
-    log.info(answer);
-    return answer;
-  }
-}
